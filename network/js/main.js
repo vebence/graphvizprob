b8c7219afaa0e6a05266e4a5a654b64d5e4fe3b8
Vetek Bence <vebence@gmail.com>
2021-07-01 16:19:00 +0200

    családtagok

--- network/js/main.js
+++ network/js/main.js
@@ -277,7 +277,7 @@ function configSigmaElements(config) {
     $GP.bg2 = $(sigInst._core.domElements.bg2);
     var a = [],
         b,x=1;
-		for (b in sigInst.clusters) a.push('<div style="line-height:12px"><a href="#' + b + '"><div style="width:40px;height:12px;border:1px solid #fff;background:' + b + ';display:inline-block"></div> Group ' + (x++) + ' (' + sigInst.clusters[b].length + ' members)</a></div>');
+		for (b in sigInst.clusters) a.push('<div style="line-height:12px"><a href="#' + b + '"><div style="width:40px;height:12px;border:1px solid #fff;background:' + b + ';display:inline-block"></div> Group ' + (x++) + ' (' + sigInst.clusters[b].length + ' fő)</a></div>');
     //a.sort();
     $GP.cluster.content(a.join(""));
     b = {
@@ -610,7 +610,7 @@ function showCluster(a) {
         sigInst.draw(2, 2, 2, 2);
         $GP.info_name.html("<b>" + a + "</b>");
         $GP.info_data.hide();
-        $GP.info_p.html("Group Members:");
+        $GP.info_p.html("Családtagok:");
         $GP.info_link.find("ul").html(f.join(""));
         $GP.info.animate({width:'show'},350);
         $GP.search.clean();
