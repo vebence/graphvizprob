ffd0980ba1fbf340ba956840807ff9bb2a7ebb46
Vetek Bence <vebence@gmail.com>
2021-07-01 14:44:39 +0200

    név

--- network/js/main.js
+++ network/js/main.js
@@ -528,13 +528,13 @@ function nodeActive(a) {
 
 	if (groupByDirection) {
 		size=Object.size(mutual);
-		f.push("<h2>Mututal (" + size + ")</h2>");
+		f.push("<h2>Házastárs (" + size + ")</h2>");
 		(size>0)? f=f.concat(createList(mutual)) : f.push("No mutual links<br>");
 		size=Object.size(incoming);
-		f.push("<h2>Incoming (" + size + ")</h2>");
+		f.push("<h2>Szülők (" + size + ")</h2>");
 		(size>0)? f=f.concat(createList(incoming)) : f.push("No incoming links<br>");
 		size=Object.size(outgoing);
-		f.push("<h2>Outgoing (" + size + ")</h2>");
+		f.push("<h2>Gyerekek (" + size + ")</h2>");
 		(size>0)? f=f.concat(createList(outgoing)) : f.push("No outgoing links<br>");
 	} else {
 		f=f.concat(createList(sigInst.neighbors));
