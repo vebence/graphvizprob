0bbd7bea70bc163481497e156d6e901a9a7d5d55
Vetek Bence <vebence@gmail.com>
2021-07-01 16:25:04 +0200

    attr

--- network/js/main.js
+++ network/js/main.js
@@ -277,7 +277,7 @@ function configSigmaElements(config) {
     $GP.bg2 = $(sigInst._core.domElements.bg2);
     var a = [],
         b,x=1;
-		for (b in sigInst.clusters) a.push('<div style="line-height:12px"><a href="#' + b + '"><div style="width:40px;height:12px;border:1px solid #fff;background:' + b + ';display:inline-block"></div> Group ' + (x++) + ' (' + sigInst.clusters[b].length + ' fő)</a></div>');
+		for (b in sigInst.clusters) a.push('<div style="line-height:12px"><a href="#' + b + '"><div style="width:40px;height:12px;border:1px solid #fff;background:' + b + ';display:inline-block"></div> ' + (attr.("család")) + ' ' + (x++) + ' (' + sigInst.clusters[b].length + ' fő)</a></div>');
     //a.sort();
     $GP.cluster.content(a.join(""));
     b = {
@@ -369,7 +369,7 @@ function Search(a) {
         this.searching = !0;
         this.lastSearch = a;
         this.results.empty();
-        if (2 >= a.length) this.results.html("<i>Legalább 3 betűt írj be a kereséshez.</i>");
+        if (2 >= a.length) this.results.html("<i>Legalább 3 karaktert írj be a kereséshez.</i>");
         else {
             sigInst.iterNodes(function (a) {
                 g.test(a.label.toLowerCase()) && c.push({
